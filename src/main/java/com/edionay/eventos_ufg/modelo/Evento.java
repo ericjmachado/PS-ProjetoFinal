4be2cea21a9772c341c2b0146ca0cba6a538c9ff
Edionay Aguiar <edionay@gmail.com>
2018-07-12 14:52:38 -0300

    Remodelando projeto para linha de comando.

---
+++ src/main/java/com/edionay/eventos_ufg/modelo/Evento.java
@@ -0,0 +1,13 @@
+package com.edionay.eventos_ufg.modelo;
+
+import java.util.Date;
+
+public class Evento {
+
+    String titulo;
+    Date dataInicial;
+    Date dataFinal;
+    String local;
+    String detalhes;
+
+}
