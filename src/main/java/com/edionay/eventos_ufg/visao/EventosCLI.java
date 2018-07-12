a2c2b86cf40e99235cb2bb8d2adfdf5d8bfb4785
Edionay Aguiar <edionay@gmail.com>
2018-07-12 16:19:35 -0300

    Classe de visão criada.

--- src/main/java/com/edionay/eventos_ufg/visao/EventosCLI.java
+++ src/main/java/com/edionay/eventos_ufg/visao/EventosCLI.java
@@ -1,4 +1,9 @@
 package com.edionay.eventos_ufg.visao;
 
 public class EventosCLI {
+
+    public static void menuPrincipal() {
+        System.out.println("Insira o número correspondente à opção desejada:");
+        System.out.println("1 - Listar eventos futuros ");
+    }
 }
